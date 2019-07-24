9e6f5a0f8376e6c60a32a6c43c4534c43bf5f87d
musesingh <musethakuri@gmail.com>
2019-07-24 16:46:59 +0545

    First Commit

---
+++ dom.js
@@ -0,0 +1,3 @@
+var divOne = document.getElementById('one').style.background = "#FF775F";
+var divTwo = document.getElementById('two').style.background = "#FF9683";
+var divThree = document.getElementById('three').style.background = "#FF9579";
